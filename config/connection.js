0ad0b1531dcd0e2af4c72d7c1f8e975957cc694d
amberorenstein <amber.orenstein@gmail.com>
2017-09-10 15:48:47 -0600

    heroku JAWS DB

--- config/connection.js
+++ config/connection.js
@@ -5,14 +5,27 @@
 // Require mysql
 var mysql = require("mysql");
 
-// Set up our connection information
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+// Set up our connection information--local server
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
+
+//JAWS DB
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'h9lllzuadkns4bmi',
+    password: 'rhwaxdwizblq9awy',
+    database: 'burgers_db'
+  });
+};
 
 // Connect to the database
 connection.connect(function(err) {
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var hbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3306;
 
 var app = express();
 app.use(express.static(__dirname + "/public"));
