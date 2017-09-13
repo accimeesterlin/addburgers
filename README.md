5f6c9ea575e2f59464cbbc317f2dbb3b26099a9c
amberorenstein <amber.orenstein@gmail.com>
2017-09-12 18:14:59 -0600

    updated rm

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # eat-da-burger
+
+Grocery list helper with MySQL, Node, Express, Handlebars, using an ORM. Project uses an MVC design pattern. Node and MySQL are used to query and route data in the app, and Handlebars was used to generate the HTML. 
---
+++ database/schemaJaws.sql
@@ -0,0 +1,14 @@
+-- Drops the burgers_db if it exists currently --
+DROP DATABASE IF EXISTS burgers_db;
+-- Creates the "burgers_db" database --
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+  id INT AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(50) NOT NULL,
+  devoured BOOLEAN NOT NULL,
+  createdAt TIMESTAMP NOT NULL,
+  PRIMARY KEY (id)
+);
